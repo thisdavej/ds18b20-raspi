6907fcb910a4cf9a503e657114ba89d4695e1a4e
b0czek <dariusz.majnert@gmail.com>
2021-02-08 00:10:10 +0100

    add non-callback versions of functions

--- index.d.ts
+++ index.d.ts
@@ -13,31 +13,37 @@ export function readC(
 	digits: number,
 	cb: (err: SensorError, result: number | null) => any
 ): any;
+export function readC(id: string, digits: number): number | null;
 
 export function readF(
 	id: string,
 	digits: number,
 	cb: (err: SensorError, result: number | null) => any
 ): any;
+export function readF(id: string, digits: number): number | null;
 
 export function readSimpleC(
 	digits: number,
 	cb: (err: SensorError, result: number | null) => any
 ): any;
+export function readSimpleC(digits: number): number | null;
 
 export function readSimpleF(
 	digits: number,
 	cb: (err: SensorError, result: number | null) => any
 ): any;
+export function readSimpleF(digits: number): number | null;
 
 export function readAllC(
 	digits: number,
 	cb: (err: string | null, result: ValueWithID[]) => any
 ): any;
+export function readAllC(digits: number): ValueWithID[];
 
 export function readAllF(
 	digits: number,
 	cb: (err: string | null, result: ValueWithID[]) => any
 ): any;
+export function readAllF(digits: number): ValueWithID[];
 
 export function setW1Directory(devicePath: string): void;
